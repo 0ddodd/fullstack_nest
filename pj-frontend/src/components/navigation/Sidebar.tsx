1b6c51fd556f40081b01ed4d9bc8b2a947528799
0ddodd <hegoeson@gmail.com>
2024-10-30 20:29:10 +0900

    m

---
+++ pj-frontend/src/components/navigation/Sidebar.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from '@clerk/clerk-react'
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div><UserButton /></div>
+    )
+}
+
+export default Sidebar
