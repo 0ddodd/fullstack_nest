59b34a831f498a93f260da274fbf99bb6c4ef701
0ddodd <hegoeson@gmail.com>
2024-11-19 16:09:01 +0900

    backend auth

--- pj-backend/src/profile/profile.resolver.ts
+++ pj-backend/src/profile/profile.resolver.ts
@@ -2,12 +2,15 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateProfileDto } from './dto';
 import { Profile } from './profile.type';
 import { ProfileService } from './profile.service';
+import { UseGuards } from '@nestjs/common';
+import { GraphqlAuthGuard } from 'src/auth/auth.guard';
 
 @Resolver()
 export class ProfileResolver {
 
     constructor(private readonly profileService: ProfileService) {}
 
+    @UseGuards(GraphqlAuthGuard)
     @Mutation(() => Profile)
     async createProfile(@Args('input') input: CreateProfileDto){
         return this.profileService.createProfile(input)
